00d5a82ba802e195a45d016040f26d626225b854
Isac Diamant <53610746+IsacDiamant@users.noreply.github.com>
2022-05-08 12:11:33 +0200

    random changes, started on room generation, this is a test push to see if I connect with network rep

---
+++ client-survival-ssf/game/RoomHandler.gd
@@ -0,0 +1,22 @@
+extends Node2D
+
+export(int) var _number_of_rooms: int 
+
+
+
+var _room_coordinates: PoolVector2Array = [Vector2(0, 0)]
+#generate_rooms generar datan, emittar sedan signal till saker som ska göra allt som behövs
+#ha en rect/bitmap och gör en set-cell utifrån den
+#array med 2d-åunkter, kolla ifall kollision med andra rum
+#is_host skickar packet med all data till clients (icl. host)
+
+
+func _ready():
+	pass 
+
+
+func _generate_rooms() -> void:
+	for i in _number_of_rooms:
+		#generate random room continuation
+		#randi() % 4
+		pass
