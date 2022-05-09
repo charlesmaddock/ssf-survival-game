7ed35a61e37f838a0c719ad446d1f77297c3b502
Charles <charlesalexander.maddock@gmail.com>
2022-05-09 13:18:47 +0200

    small change for mutliplayer

--- client-survival-ssf/components/ChainsawPart.gd
+++ client-survival-ssf/components/ChainsawPart.gd
@@ -6,12 +6,13 @@ var able_to_attack: bool = true
 
 
 func _input(event):
-	rotation = global_position.angle_to_point(get_global_mouse_position()) + PI
-	
-	if Input.is_action_just_pressed("attack") && parent_entity.id == Lobby.my_id && able_to_attack == true:
-		able_to_attack = false
-		var dir = (get_global_mouse_position() - global_position).normalized()
-		Server.melee_attack(parent_entity.id, dir, parent_entity.team)
+	if parent_entity.id == Lobby.my_id:
+		rotation = global_position.angle_to_point(get_global_mouse_position()) + PI
+		
+		if Input.is_action_just_pressed("attack") && able_to_attack == true:
+			able_to_attack = false
+			var dir = (get_global_mouse_position() - global_position).normalized()
+			Server.melee_attack(parent_entity.id, dir, parent_entity.team)
 
 
 func _on_Timer_timeout():
