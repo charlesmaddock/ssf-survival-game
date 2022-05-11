4b591f284fcb766c2f179fbc52bc3a9e1d2b68bc
Charles <charlesalexander.maddock@gmail.com>
2022-05-11 20:26:11 +0200

    new url

--- builds/app.js
+++ builds/app.js
@@ -4,7 +4,7 @@ const path = require("path");
 const app = express();
 const port = process.env.PORT || 8081;
 
-app.use("/", express.static(path.join(__dirname, "public")));
+app.use("/ssf-heroes", express.static(path.join(__dirname, "public")));
 
 app.listen(port);
 console.log("Server started at http://localhost:" + port);
