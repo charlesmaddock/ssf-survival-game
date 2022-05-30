737388e6d1803e7e4b422e3d9d3d95bc8d70cefa
Isac Diamant <53610746+IsacDiamant@users.noreply.github.com>
2022-05-31 00:04:02 +0200

    Making last commit before creating upstream branch. WARNING: This branch has a boss spawn in the first room - this can be removed by simple edits in the generate_mobs-function. Changes: RomansBoss' hands now work properly. I will make this boss easily spawnable in the last room - however some changes to spawn_pos of mobs are due in order for this to happen. I hope to complete this tomorrow.

--- client-survival-ssf/entities/RomansBossHand.gd
+++ client-survival-ssf/entities/RomansBossHand.gd
@@ -7,6 +7,7 @@ export var _charge_speed: float = 400
 export var _time_to_charge: float = 2.0
 export var _chargeback_speed: float = 100
 export var _time_to_chargeback: float = 0.8
+export var _time_before_charge_is_available_again: float = 1.5
 
 onready var AI_node: Node2D = self.get_node("AI")
 onready var health_node: Node2D = self.get_node("Health")
@@ -16,6 +17,7 @@ onready var raycast_player_detection: Node2D = self.get_node("RayCastPlayerDetec
 onready var raycast_wall_detection: Node2D = self.get_node("RayCastWallDetection")
 onready var timer_to_charge: Timer = self.get_node("TimerToCharge")
 onready var timer_to_chargeback: Timer = self.get_node("TimerToChargeback")
+onready var timer_before_charge_is_available: Timer = self.get_node("TimerBeforeChargeIsAvailable")
 
 
 var entity: Entity
@@ -25,7 +27,6 @@ var _is_left_hand: bool = true
 
 var _passive_position: Vector2
 var _strafe_center_pos: Vector2
-var _is_first_strafe_pos: bool = true
 var _strafe_direction: int = 1
 
 var _behaviour_state: int = behaviourState.HOVER
@@ -42,13 +43,16 @@ func init(left_hand: bool, spawn_pos: Vector2):
 	_passive_position = spawn_pos
 	_is_left_hand = left_hand
 	
+	print("This is the current passive pos: ", _passive_position)
+	
 	if !_is_left_hand:
 		_strafe_direction = -1
 	
 	_init_finished = true
 
 func _ready():
-	
+	timer_before_charge_is_available.set_wait_time(_time_before_charge_is_available_again)
+	timer_before_charge_is_available.start()
 	timer_to_charge.set_wait_time(_time_to_charge)
 	timer_to_chargeback.set_wait_time(_time_to_chargeback)
 	
@@ -60,42 +64,42 @@ func _process(delta):
 	if Lobby.is_host == true && _init_finished: 
 		
 		if _behaviour_state == behaviourState.HOVER: 
-			print("I am in hover hover mode! :" , entity.id)
-			if raycast_player_detection.is_colliding_with_layers(Constants.collisionLayers.PLAYER):
-				print("Player detected in hover mode")
+			if raycast_player_detection.is_colliding_with_layers([Constants.collisionLayers.PLAYER]).size() != 0 && timer_before_charge_is_available.is_stopped():
+				print("Calling charge")
 				_charge_mode()
 			else:
 				_hover_movement(delta)
 		
 		elif _behaviour_state == behaviourState.CHARGE:
-			
 			if timer_to_charge.is_stopped():
-				if raycast_wall_detection.is_colliding_with_layers(Constants.collisionLayers.SOLID):
+				if raycast_wall_detection.is_colliding_with_layers([Constants.collisionLayers.SOLID]):
+					print("calling chargeback")
 					_chargeback_mode()
 		
 		elif _behaviour_state == behaviourState.CHARGEBACK:
-			if self.global_position.distance_to(_passive_position) <= 10:
+			if self.global_position.distance_to(_passive_position) <= 20:
+				print("calling hover mode")
+				timer_before_charge_is_available.start()
 				_hover_mode()
+
 
 func _hover_movement(delta: float) -> void:
 	var up_or_down_start: Vector2
 	
-	if _is_first_strafe_pos:
-		if _is_left_hand:
-			up_or_down_start = Vector2(0.0, 0.1)
-		else:
-			up_or_down_start = -Vector2(0.0, 0.1)
-		_is_first_strafe_pos = false
+	if _is_left_hand:
+		up_or_down_start = Vector2(0.0, 0.1)
+	else:
+		up_or_down_start = -Vector2(0.0, 0.1)
 	
 	var strafe_center_point = _passive_position + up_or_down_start
 	var strafe_dir_normalized = strafe_center_point.direction_to(self.global_position).normalized()
-	
 	
 	var strafe_pos: Vector2  = strafe_center_point + (strafe_dir_normalized.rotated(deg2rad(_strafe_direction * _strafe_speed * delta)) * _strafe_dist)
 	self.global_position = strafe_pos
 
 
 func _hover_mode() -> void:
+	AI_node.motionless_behaviour()
 	_behaviour_state = behaviourState.HOVER
 
 
@@ -103,17 +107,22 @@ func _charge_mode() -> void:
 	_behaviour_state = behaviourState.CHARGE
 	timer_to_charge.start()
 
+
 func _chargeback_mode() -> void:
-	_behaviour_state = behaviourState.CHARGEBACK
-	AI_node.stop_moving()
+	AI_node.motionless_behaviour()
 	timer_to_chargeback.start()
+	_behaviour_state = behaviourState.CHARGEBACK
 
 
 func _on_TimerToCharge_timeout():
 	entity.emit_signal("change_movement_speed", _charge_speed)
+	AI_node.custom_behaviour()
 	AI_node.set_target_walking_path(self.global_position + Vector2(0, 1000))
 
 
 func _on_TimerToChargeback_timeout():	
 	entity.emit_signal("change_movement_speed", _chargeback_speed)
+	print("This is the pos im moving to now! :", _passive_position)
+	AI_node.custom_behaviour()
+	yield(get_tree().create_timer(0.1), "timeout")
 	AI_node.set_target_walking_path(_passive_position)
